e5873e9c3326057364fef49f08a9a6a4a4920e27
Robindiddams <robindiddams@gmail.com>
2018-02-14 17:37:40 -0500

    Initial Commit

---
+++ emojiquiz/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  emojiquiz
+//
+//  Created by Robin Diddams on 2/14/18.
+//  Copyright © 2018 Robin Diddams. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
